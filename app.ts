911688dbaa046159fe594539c1f864fda1db9df6
Kao <kaobrent@gmail.com>
2024-05-20 11:19:43 +0800

    Add: rate-limit

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ console.log("Hello World!");
 import express, { Express, Application } from "express";
 import "express-async-errors"; 
 import cors from "cors";
+import { rateLimit } from 'express-rate-limit'
 import routes from "./app/routes";
 import { env } from "./env";
 import { errorHandler } from "./app/middlewares/errors";
@@ -9,6 +10,15 @@ import { errorHandler } from "./app/middlewares/errors";
 const app: Express = express();
 const port: number = env.PORT;
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+  skip: (req, res) => ["/"].includes(req.url) // Bypass the rate limit for some requests
+})
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
