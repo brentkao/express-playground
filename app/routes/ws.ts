d8cfcb833af4eef249a63f887cdc9786c8962df9
Kao <kaobrent@gmail.com>
2024-06-20 12:14:14 +0800

    Update: 開放 cors (localhost)

--- app/routes/ws.ts
+++ app/routes/ws.ts
@@ -6,8 +6,29 @@ import { wsJWT, roleCheck } from "../middlewares/jwt";
 import c_Server from "../constants/server";
 import { wsGameConnection, normalConnection } from "../controller/websocket";
 import { validateGameToken } from "../middlewares/ws-game";
+import cors from "cors";
 
+const allowedOrigins = ["http://localhost", "http://127.0.0.1"];
 export default function (app: Express) {
+  //TODO: 暫時允許所有來源, 之後要改成只允許特定來源
+  //➫ 設定 CORS
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (
+          !origin ||
+          allowedOrigins.some((allowedOrigin) =>
+            origin.startsWith(allowedOrigin)
+          )
+        ) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
+    })
+  );
   //➫ 初始化 express-ws 並套用至 app
   expressWs(app);
   const router = express.Router();
